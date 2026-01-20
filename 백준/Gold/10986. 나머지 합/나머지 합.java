ce0d7458d8c1fabd11677c5c5d8ae02d703c7c82
Dozzang <74997120+dozzzang@users.noreply.github.com>
2026-01-20 18:28:21 +0900

    [Gold III] Title: 나머지 합, Time: 380 ms, Memory: 120784 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,18 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         long[] S = new long[N];
         long[] C = new long[M];
         long answer = 0;
 
-        S[0] = sc.nextLong();
+        st = new StringTokenizer(br.readLine());
+        S[0] = Long.parseLong(st.nextToken());
+
         for(int i = 1; i < N; i++) {
-            S[i] = S[i - 1] + sc.nextLong();
+            S[i] = S[i - 1] + Long.parseLong(st.nextToken());
         }
         for(int i = 0; i < N; i++) {
             int remainder = (int)(S[i] % M);
@@ -26,4 +33,3 @@ public class Main{
         System.out.println(answer);
     }
 }
-
