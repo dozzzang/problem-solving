3fa05932b1b0294e4c9f422a5ac2e06a18dc61a4
Dozzang <74997120+dozzzang@users.noreply.github.com>
2026-04-05 23:15:07 +0900

    [Silver I] Title: 오르막 수, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+import java.lang.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[n + 1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % 10007;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = (sum + dp[n][i]) % 10007;
+        }
+        System.out.print(sum);
+    }
+}
